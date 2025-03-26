60bf3ed61c5284aad87884c2062777af0dc13fdf
tilaplapya <vinzreibhenilagan@gmail.com>
2025-03-26 22:35:32 +0800

    activity 4

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE':'django.db.backends.postgresql', 
+    'NAME': 'sampledb',
+    'USER': 'myuser', 
+    'PASSWORD': 123,
+    'HOST': 'localhost',
+    'PORT': '5432',
+        }
 }
 
 
